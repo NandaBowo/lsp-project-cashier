b366d33b2cf00534b08ab7dfefc64eaca1ade379
NandaBowo <nandabowo.nb@gmail.com>
2021-04-09 15:00:16 +0700

    Initial commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::query()->create([
+            'name' => 'Aqua',
+            'price' => 3000,
+            'quantity' => 100,
+            'image_url' => 'products/aqua.jpg',
+        ]);
+        Product::query()->create([
+            'name' => 'Coca-Cola',
+            'price' => 5000,
+            'quantity' => 100,
+            'image_url' => 'products/coca-cola.jpg',
+        ]);
+        Product::query()->create([
+            'name' => 'Mizone',
+            'price' => 7000,
+            'quantity' => 100,
+            'image_url' =>  'products/mizone.jpg',
+        ]);
+    }
+}
